2430ad43cf753db1441f2385883efa120722308c
ericburdett <eric@burdett.us>
2019-03-16 14:19:52 -0600

    Added express to serve static files. Home page is now accessible on heroku.

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 const WebSocket = require('ws');
+const express = require('express');
+const app = express();
 
 const PORT = process.env.PORT || 5000;
-var wss = new WebSocket.Server({port:PORT});
 
+// Use Express for static pages
+var server = app.use(express.static('public')).listen(PORT);
 console.log("Listening for connections on port " + PORT + "...");
 
-wss.broadcast = function(data) {
-    console.log("in broadcast");
+// Use web sockets for signaling
+var wss = new WebSocket.Server({ server: server});
 
+// Broadcast message to all clients connected to server
+wss.broadcast = function(data) {
     this.clients.forEach((client) => {
-        console.log("sending...");
         client.send(data);
     });
 };
 
+// Handler for incoming connections
 wss.on('connection', function(ws) {
     ws.on('message', function(message) {
         console.log('received');
-        // console.log('received: %s', message);
+        console.log('received: %s', message);
         wss.broadcast(message);
     });
 });
