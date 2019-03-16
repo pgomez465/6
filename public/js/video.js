bc589813582d32e8dee4d3e52e5c0810016cdab4
ericburdett <eric@burdett.us>
2019-03-16 14:37:26 -0600

    Attempt at fixing problem...

--- public/js/video.js
+++ public/js/video.js
@@ -1,4 +1,10 @@
 'use strict';
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
+window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
+
 var localStream;
 var localVideo;
 var remoteVideo;
